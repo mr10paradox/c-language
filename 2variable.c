7d2c9ebe91c4871a84aed5cedee6f28a733b7f82
mr10paradox <hrao8700@gmail.com>
2023-08-31 20:43:47 +0530

    adding new file

---
+++ 2variable.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+
+int main()
+{
+    // initiliazing variables 
+    int a = 4;
+    float b = 8.5;
+    char c = "u";
+    int d = 45;
+    // Here int,float,char are the datatypes. 
+    printf("The value of a is %d \n", a);
+    printf("The value of b is %f \n", b);
+    printf("The value of c is %c \n", c);
+    printf("the sum of a and d is %d \n", a-d);
+    return 0; 
+}
---
+++ 3input.c
@@ -0,0 +1,17 @@
+// program to find the sum by taking input from user.
+# include<stdio.h>
+
+int main()
+{
+  // initializing variables 
+  int a, b;
+
+  printf("Enter the value of a\n");
+  scanf("%d", &a);// taking input from user.
+
+  printf("Enter the value of b\n");
+  scanf("%d", &b);
+
+  printf("The sum of a and b is %d", a+b); 
+    return 0; 
+}
